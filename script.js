995dcd0afd89a9901910fcf91d0075934ed9585c
JoaoLucasLourenco <jao.lucas.lourenco@gmail.com>
2023-06-25 11:29:39 -0300

    AllClassesIncluded

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 function flex(id,btn){
     var botao = document.getElementById(btn);
-    if(document.getElementById(id).style.display == "flex")
+    if(document.getElementById(id).style.display == "block")
     {
         document.getElementById(id).style.display ="none"
         botao.textContent = 'Mostrar Mais >'
     }
     else{
-        document.getElementById(id).style.display ="flex"
+        document.getElementById(id).style.display ="block"
         botao.textContent = 'Mostrar Menos >'}
+}
+function dado(d,classe){
+    dclasse = document.getElementById(classe)
+    const min = 1
+    const max = parseInt(d,10)
+    const randomNum = Math.floor(Math.random() * (max - min + 1)) + min
+    dclasse.textContent = randomNum
 }
   
